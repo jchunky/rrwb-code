0a02cc2f36a4e402267d7223a634428537eadd42
Chunky <jasonc@nulogy.com>
2021-04-02 14:26:29 -0400

    refactor

--- template/template.rb
+++ template/template.rb
@@ -1,10 +1,9 @@
 module Template
-  def template(template, req_id)
-    code = req_id
-    altcode = "#{code[0..4]}-#{code[5..7]}"
+  def template(template, code)
+    alt_code = [code[0..4], code[5..7]].join("-")
 
     template
       .gsub("%CODE%", code)
-      .gsub("%ALTCODE%", altcode)
+      .gsub("%ALTCODE%", alt_code)
   end
 end
