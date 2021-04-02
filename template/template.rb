9efe220ada2c2a70405b5202196207fd63ed040e
Chunky <jasonc@nulogy.com>
2021-04-02 14:21:41 -0400

    refactor

--- template/template.rb
+++ template/template.rb
@@ -1,13 +1,6 @@
 module Template
-  def template(source_template, req_id)
-    template = String.new(source_template)
-
-    # Substitute for %CODE%
-    template_split_begin = template.index("%CODE%")
-    template_split_end = template_split_begin + 6
-    template_part_one = String.new(template[0..(template_split_begin - 1)])
-    template_part_two = String.new(template[template_split_end..template.length])
-    code = String.new(req_id)
+  def template(template, req_id)
+    code = req_id
     altcode = "#{code[0..4]}-#{code[5..7]}"
 
     template
