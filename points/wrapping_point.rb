9606d934b877f4c6d18fc79945d04e6180545b11
Chunky <jasonc@nulogy.com>
2021-04-04 09:57:49 -0400

    refactor

--- points/wrapping_point.rb
+++ points/wrapping_point.rb
@@ -1,18 +1,20 @@
+require "active_support/all"
+
 class WrappingPoint
-  attr_reader :x, :y, :max_x, :max_y
+  attr_reader :point, :max
+
+  delegate :x, :y, to: :point
 
   def initialize(x, y, max_x, max_y)
-    @x = x % max_x
-    @y = y % max_y
-    @max_x = max_x
-    @max_y = max_y
+    @max = Point.new(max_x, max_y)
+    @point = Point.new(x, y) % @max
   end
 
   def move_to(x, y)
-    self.class.new(x, y, max_x, max_y)
+    self.class.new(x, y, max.x, max.y)
   end
 
-  def move_by(point)
-    self.class.new(x + point.x, y + point.y, max_x, max_y)
+  def move_by(other)
+    self.class.new(x + other.x, y + other.y, max.x, max.y)
   end
 end
