3123d408ea7876d898aeb9c3a58a499a7932f30b
Chunky <jasonc@nulogy.com>
2021-05-02 03:52:31 -0400

    refactor

--- template/template_test.rb
+++ template/template_test.rb
@@ -1,11 +1,11 @@
-require 'test/unit'
-require_relative 'template'
+require "test/unit"
+require_relative "template"
 
 class TemplateTest < Test::Unit::TestCase
   include Template
 
   def test_missing_interval
-    puts 'Expected output: Code is 5678901234; alt code is 56789-012'
-    template 'Code is %CODE%; alt code is %ALTCODE%', '5678901234'
+    puts "Expected output: Code is 5678901234; alt code is 56789-012"
+    template "Code is %CODE%; alt code is %ALTCODE%", "5678901234"
   end
 end
