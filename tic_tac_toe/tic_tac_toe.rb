9b81e2984d647461146fc860121a076bd5f969d2
Chunky <jasonc@nulogy.com>
2021-05-02 17:15:15 -0400

    refactor

--- tic_tac_toe/tic_tac_toe.rb
+++ tic_tac_toe/tic_tac_toe.rb
@@ -1,40 +1,53 @@
 class Game
   attr_accessor :board
 
-  def initialize(s, position=nil, player=nil)
-    @board = s.dup
-    @board[position] = player unless position == nil
+  def initialize(board, position = nil, player = nil)
+    @board = board.clone
+    @board[position] = player if position
   end
 
   def move(player)
-    (0..8).each do |i|
-      if board[i,1] == '-'
-        game = play(i, player)
-        return i if game.winner() == player
-      end
+    find_winning_position(player) || board.index("-") || -1
+  end
+
+  def winner
+    3.times.each do |i|
+      win = winner_at_location(col(i))
+      return win if win
+    end
+    3.times.each do |i|
+      win = winner_at_location(row(i))
+      return win if win
     end
 
-    (0..8).each { |i| return i if board[i,1] == '-' }
-    return -1
+    "-"
+  end
+
+  private
+
+  def winner_at_location(squares)
+    squares.uniq.one? && squares.first != "-" && squares.first
+  end
+
+  def col(i)
+    [board[i], board[i + 3], board[i + 6]]
+  end
+
+  def row(i)
+    board.chars[i * 3, 3]
+  end
+
+  def find_winning_position(player)
+    board.chars.each_with_index do |square, i|
+      next unless square == "-"
+
+      game = play(i, player)
+      return i if game.winner == player
+    end
+    nil
   end
 
   def play(i, player)
     Game.new(board, i, player)
-  end
-
-  def winner
-    if board[0,1] != '-' && board[0,1] == board[1,1] &&
-        board[1,1] == board[2,1]
-      return board[0,1]
-    end
-    if board[3,1] != '-' && board[3,1] == board[4,1] &&
-        board[4,1] == board[5,1]
-      return board[3,1]
-    end
-    if board[6,1] != '-' && board[6,1] == board[7,1] &&
-        board[7,1] == board[8,1]
-      return board[6,1]
-    end
-    return '-'
   end
 end
