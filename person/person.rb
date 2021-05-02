9c0a6e09b716a497e3ca9065ee9a8a3f59c22499
Chunky <jasonc@nulogy.com>
2021-05-02 12:47:33 -0400

    refactor

--- person/person.rb
+++ person/person.rb
@@ -1,1 +1,9 @@
-Person = Struct.new('Person', :last, :first, :middle)
+class Person < Struct.new(:last, :first, :middle)
+  def full_name
+    [first, middle, last].compact.join(" ")
+  end
+
+  def full_name_variant
+    ["#{last},", first, middle].compact.join(" ")
+  end
+end
--- person/person_test.rb
+++ person/person_test.rb
@@ -1,42 +1,22 @@
-require 'stringio'
-require 'test/unit'
-
-require 'person'
+require "stringio"
+require "test/unit"
+require_relative "person"
 
 class PersonClient < Test::Unit::TestCase
-
   def client1(out, person)
-    out.write(person.first)
-    out.write(" ")
-    if person.middle != nil
-      out.write(person.middle)
-      out.write(" ")
-    end
-    out.write(person.last)
+    out << person.full_name
   end
 
   def client2(person)
-    result = person.last + ", " + person.first
-    if (person.middle != nil)
-      result += " " + person.middle
-		end
-    return result
+    person.full_name_variant
   end
 
   def client3(out, person)
-    out.write(person.last)
-    out.write(", ")
-    out.write(person.first)
-    if (person.middle != nil)
-      out.write(" ")
-      out.write(person.middle)
-    end
+    out << person.full_name_variant
   end
 
   def client4(person)
-    return person.last + ", " +
-         person.first +
-         ((person.middle == nil) ? "" : " " + person.middle)
+    person.full_name_variant
   end
 
   def test_clients
