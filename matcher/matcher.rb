de2c78a3a13993f19ee46f5e4babed788fa806d3
Chunky <jasonc@nulogy.com>
2021-05-02 06:01:31 -0400

    refactor

--- matcher/matcher.rb
+++ matcher/matcher.rb
@@ -1,19 +1,18 @@
 class Matcher
   def match(expected, actual, clip_limit, delta)
-    actual = apply_clip_limit(actual, clip_limit)
-    return false unless actual.size == expected.size
-    return false unless values_within_delta?(expected, actual, delta)
+    actual = clip(actual, clip_limit)
 
-    true
+    actual.size == expected.size &&
+      similar_values?(expected, actual, delta)
   end
 
   private
 
-  def apply_clip_limit(values, clip_limit)
+  def clip(values, clip_limit)
     values.map { |val| [val, clip_limit].min }
   end
 
-  def values_within_delta?(values1, values2, delta)
+  def similar_values?(values1, values2, delta)
     values1.zip(values2).all? { |v1, v2| (v1 - v2).abs <= delta }
   end
 end
