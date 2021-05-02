333221b4be7694565ddc04eaab3ea9b3031017c3
Chunky <jasonc@nulogy.com>
2021-05-02 13:51:41 -0400

    Update seed_ruby

---
+++ theater/test_helper.rb
@@ -0,0 +1,6 @@
+require "active_support/all"
+relative_load_paths = %w[lib]
+ActiveSupport::Dependencies.autoload_paths += relative_load_paths
+
+# require "minitest/reporters"
+# Minitest::Reporters.use!
