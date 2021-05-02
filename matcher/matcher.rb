fe3bf8b99294fdbb3401f2abc30e2b6cb3e78c86
Chunky <jasonc@nulogy.com>
2021-05-02 15:40:50 -0400

    refactor

--- matcher/matcher.rb
+++ matcher/matcher.rb
@@ -1,20 +1,18 @@
+class Array
+  def clip(clip_limit)
+    map { |val| [val, clip_limit].min }
+  end
+
+  def similar?(other, delta)
+    size == other.size &&
+      zip(other).all? { |v1, v2| (v1 - v2).abs <= delta }
+  end
+end
+
 module Matcher
   extend self
 
   def match(expected, actual, clip_limit, delta)
-    actual = clip(actual, clip_limit)
-
-    actual.size == expected.size &&
-      similar_values?(expected, actual, delta)
-  end
-
-  private
-
-  def clip(values, clip_limit)
-    values.map { |val| [val, clip_limit].min }
-  end
-
-  def similar_values?(values1, values2, delta)
-    values1.zip(values2).all? { |v1, v2| (v1 - v2).abs <= delta }
+    actual.clip(clip_limit).similar?(expected, delta)
   end
 end
