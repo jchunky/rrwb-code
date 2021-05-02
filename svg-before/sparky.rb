94a839acfd5d7ae8ac9492edd15995e45836fc1b
Chunky <jasonc@nulogy.com>
2021-05-02 02:54:42 -0400

    refactor

--- svg-before/sparky.rb
+++ svg-before/sparky.rb
@@ -1,40 +1,85 @@
-NUMBER_OF_TOSSES = 1000
-BORDER_WIDTH = 50
-
-def toss
-  2 * (rand(2)*2 - 1)
+class Point < Struct.new(:x, :y)
+  def to_s
+    [x, y].join(",")
+  end
 end
 
-def values(n)
-  a = [0]
-  n.times { a << (toss + a[-1]) }
-  a
+module DataGenerator
+  extend self
+
+  NUMBER_OF_TOSSES = 1000
+
+  def points
+    values.map.with_index { |value, i| Point.new(i, value) }
+  end
+
+  private
+
+  def values
+    NUMBER_OF_TOSSES.times.each_with_object([0]) do |_, acc|
+      acc << acc.last + toss
+    end
+  end
+
+  def toss
+    [-1, 1].sample
+  end
 end
 
-def spark(centre_x, centre_y, value)
-  "<rect x=\"#{centre_x-2}\" y=\"#{centre_y-2}\"
-    width=\"4\" height=\"4\"
-    fill=\"red\" stroke=\"none\" stroke-width=\"0\" />
-  <text x=\"#{centre_x+6}\" y=\"#{centre_y+4}\"
-    font-family=\"Verdana\" font-size=\"9\"
-    fill=\"red\" >#{value}</text>"
+module Sparkline
+  extend self
+
+  def render_svg(points)
+    Svg.render do |svg|
+      [
+        svg.line(x1: 0, y1: 0, x2: points.size, y2: 0),
+        svg.sparkline(points),
+        svg.spark(x: points.size - 1, y: points.last.y, value: points.last.y),
+      ]
+    end
+  end
 end
 
-$tosses = values(NUMBER_OF_TOSSES)
-points = []
-$tosses.each_index { |i| points << "#{i},#{200-$tosses[i]}" }
+module Svg
+  extend self
 
-data = "<svg xmlns=\"http://www.w3.org/2000/svg\"
-     xmlns:xlink=\"http://www.w3.org/1999/xlink\" >
-  <!-- x-axis -->
-  <line x1=\"0\" y1=\"200\" x2=\"#{NUMBER_OF_TOSSES}\" y2=\"200\"
-            stroke=\"#999\" stroke-width=\"1\" />
-  <polyline fill=\"none\" stroke=\"#333\" stroke-width=\"1\"
-    points = \"#{points.join(' ')}\" />
-  #{spark(NUMBER_OF_TOSSES-1, 200-$tosses[-1], $tosses[-1])}
-</svg>"
+  Y_OFFSET = 200
 
-puts "Content-Type: image/svg+xml
-Content-Length: #{data.length}
+  def render
+    %(
+      <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" >
+        #{yield(self).join}
+      </svg>
+    )
+  end
 
-#{data}"
+  def line(x1:, y1:, x2:, y2:)
+    %(<line x1="#{x1}" y1="#{normalize_y(y1)}" x2="#{x2}" y2="#{normalize_y(y2)}" stroke="#999" stroke-width="1" />)
+  end
+
+  def sparkline(points)
+    %(<polyline points="#{normalize(points).join(' ')}" fill="none" stroke="#333" stroke-width="1" />)
+  end
+
+  def spark(x:, y:, value:)
+    %(
+      <rect x="#{x - 2}" y="#{normalize_y(y) - 2}" width="4" height="4" fill="red" stroke="none" stroke-width="0" />
+      <text x="#{x + 6}" y="#{normalize_y(y) + 4}" font-family="Verdana" font-size="9" fill="red" >
+        #{value}
+      </text>
+    )
+  end
+
+  private
+
+  def normalize(points)
+    points.map { |point| Point.new(point.x, normalize_y(point.y)) }
+  end
+
+  def normalize_y(y)
+    Y_OFFSET - y
+  end
+end
+
+points = DataGenerator.points
+puts Sparkline.render_svg(points)
